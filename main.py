11ea51a9a0ad61cd0c18b19176cf4a55bb68279d
bserna13 <101461524+bserna13@users.noreply.github.com>
2022-03-22 16:13:42 -0500

    Update main.py

--- main.py
+++ main.py
@@ -17,8 +17,8 @@ class Asiento:
             self.color="blanco"
         
 class Auto:
-    cantidadCreados=0
-    
+    cantidadCreados=0.0
+
     def __init__(self,modelo,precio,asientos,marca,motor,registro):
         self.modelo=modelo
         self.precio=precio
